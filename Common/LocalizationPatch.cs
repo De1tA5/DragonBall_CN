386c823612c7fc755af5a1fd66c86d75e5323ac1
De1tA5 <1303745051@qq.com>
2026-08-29 15:38:26 +0800

    硬编码部分汉化

---
+++ Common/LocalizationPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using TigerForceLocalizationLib;
+
+namespace DragonBall_CN.Common
+{
+    public class LocalizationPatch:ModSystem
+    {
+        private readonly string dragonBall = "DBZMODPORT";
+        public override void PostSetupContent()
+        {
+            if (ModLoader.HasMod(dragonBall)) 
+            {
+                TigerForceLocalizationHelper.LocalizeAll(Mod.Name, dragonBall, false);
+            }
+        }
+    }
+}
