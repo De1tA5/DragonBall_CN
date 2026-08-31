dcba57eed9b3e3c328bc1eb596eac19011c336c3
De1tA5 <1303745051@qq.com>
2026-08-31 17:11:29 +0800

    提升可读性 & 修复tile MapEntry 缺失

--- Common/LocalizationPatch.cs
+++ Common/LocalizationPatch.cs
@@ -1,21 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Terraria.ModLoader;
 using TigerForceLocalizationLib;
+using Microsoft.Xna.Framework;
+using Terraria.Localization;
 
 namespace DragonBall_CN.Common
 {
-    public class LocalizationPatch:ModSystem
+    public class LocalizationPatch : ModSystem
     {
         private readonly string dragonBall = "DBZMODPORT";
         public override void PostSetupContent()
         {
-            if (ModLoader.HasMod(dragonBall)) 
+            if (ModLoader.HasMod(dragonBall))
             {
                 TigerForceLocalizationHelper.LocalizeAll(Mod.Name, dragonBall, false);
+            }
+        }
+    }
+
+
+    public class TileEntryPatch : ModSystem
+    {
+        //仅修复制作站tile
+        public override void Load()
+        {
+            if (!ModLoader.TryGetMod("DBZMODPORT", out Mod mod))
+                return;
+
+            ModTile? zTableTile = mod.Find<ModTile>("ZTable");
+            ModTile? kaiTableTile = mod.Find<ModTile>("KaiTable");
+
+            if (zTableTile is not null)
+            {
+                LocalizedText name = zTableTile.CreateMapEntryName();
+                zTableTile.AddMapEntry(new Color(255, 250, 34),name);
+            }
+
+            if (kaiTableTile is not null) 
+            {
+                LocalizedText name = kaiTableTile.CreateMapEntryName();
+                kaiTableTile.AddMapEntry(new Color(115, 204, 32), name);
             }
         }
     }
