383f48f95792c4993079946a8361ed69ee8077b9
De1tA5 <1303745051@qq.com>
2026-09-22 22:10:25 +0800

    dbzmodfargos 部分汉化 & 龙珠本体 汉化优化

---
+++ Common/dbzmodfargos/dbzmodfargosPatch.cs
@@ -0,0 +1,22 @@
+﻿using Terraria.ModLoader;
+using TigerForceLocalizationLib;
+using TigerForceLocalizationLib.Filters;
+
+namespace DragonBall_CN.Common.dbzmodfargos
+{
+    public class dbzmodfargosPatch : ModSystem
+    {
+        private readonly static string DBZF = "dbzmodfargos";
+
+        public override void PostSetupContent()
+        {
+            if (ModLoader.HasMod(DBZF))
+            {
+                TigerForceLocalizationHelper.LocalizeAll(Mod.Name, DBZF, false, filters: new() 
+                {
+                    MethodFilter = MethodFilter.MatchNames("OnKill", "UpdateArmorSet")
+                });
+            }
+        }
+    }
+}
